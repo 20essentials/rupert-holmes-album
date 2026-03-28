2529afa64e425a49e3cea4531c42e3eda74e18f3
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 14:25:09 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RUPERT HOLMES';
+export let nameSongs = [
+	"ESCAPE", /*1*/
+	"HIM", /*2*/
+	"TERMINAL", /*3*/
+	"WHO, WHAT, WHEN, WHERE, WHY", /*4*/
+	"DECO LADY", /*5*/
+	"THE PEOPLE THAT YOU NEVER GET TO LOVE", /*6*/
+	"IN YOU I TRUST", /*7*/
+	"PARTNERS IN CRIME", /*8*/
+	"LUNCH HOUR", /*9*/
+	"NEARSIGHTED", /*10*/
+	"ANSWERING MACHINE", /*11*/
+	"SO BEAUTIFUL IT HURTS", /*12*/
+	"SPEECHLESS", /*13*/
+	"TOUCH AND GO", /*14*/
+	"WEEKEND LOVER", /*15*/
+	"PERFECT", /*16*/
+	"LESS IS MORE", /*17*/
+	"ADVENTURE", /*18*/
+	"BLACKJACK", /*19*/
+	"THE O'BRIEN GIRL", /*20*/
+]
